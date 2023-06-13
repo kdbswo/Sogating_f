6831e43363fef6a5b1fcae4c23374204ac34fe0f
kdbswo <kdbswo@gmail.com>
2023-06-14 02:57:22 +0900

    Add getUserData Logic

--- app/src/main/java/com/example/sogating_f/MainActivity.kt
+++ app/src/main/java/com/example/sogating_f/MainActivity.kt
@@ -6,13 +6,19 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.example.sogating_f.auth.UserDataModel
 import com.example.sogating_f.setting.SettingActivity
 import com.example.sogating_f.slider.CardStackAdapter
+import com.example.sogating_f.utils.FirebaseAuthUtils
 import com.example.sogating_f.utils.FirebaseRef
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.ktx.storage
 import com.yuyakaido.android.cardstackview.CardStackLayoutManager
 import com.yuyakaido.android.cardstackview.CardStackListener
 import com.yuyakaido.android.cardstackview.CardStackView
@@ -26,6 +32,13 @@ class MainActivity : AppCompatActivity() {
     lateinit var manager: CardStackLayoutManager
 
     private val userDataList = mutableListOf<UserDataModel>()
+
+    private var userCount = 0
+
+    private val uid = FirebaseAuthUtils.getUid()
+
+    private lateinit var currentUserGender : String
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -43,10 +56,23 @@ class MainActivity : AppCompatActivity() {
 
         manager = CardStackLayoutManager(baseContext, object : CardStackListener {
             override fun onCardDragging(direction: Direction?, ratio: Float) {
-
             }
 
             override fun onCardSwiped(direction: Direction?) {
+
+                if (direction == Direction.Right) {
+                    Toast.makeText(this@MainActivity, "right", Toast.LENGTH_SHORT).show()
+                }
+                if (direction == Direction.Left) {
+                    Toast.makeText(this@MainActivity, "left", Toast.LENGTH_SHORT).show()
+                }
+
+                userCount += 1
+
+                if (userCount == userDataList.count()) {
+                    getUserDataList(currentUserGender)
+                    Toast.makeText(this@MainActivity, "유저 새로 받기", Toast.LENGTH_SHORT).show()
+                }
 
             }
 
@@ -68,19 +94,39 @@ class MainActivity : AppCompatActivity() {
 
         })
 
-        val testList = mutableListOf<String>()
-        testList.add("a")
-        testList.add("b")
-        testList.add("c")
-
         cardStackAdapter = CardStackAdapter(baseContext, userDataList)
         cardStackView.layoutManager = manager
         cardStackView.adapter = cardStackAdapter
 
-        getUserDataList()
+//        getUserDataList()
+        getMyUserData()
     }
 
-    private fun getUserDataList() {
+    private fun getMyUserData() {
+
+        val postListener = object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+
+//                Log.w(TAG, dataSnapshot.toString())
+                val data = dataSnapshot.getValue(UserDataModel::class.java)
+
+                Log.w(TAG, data?.gender.toString())
+
+                currentUserGender = data?.gender.toString()
+
+                getUserDataList(currentUserGender)
+
+            }
+
+            override fun onCancelled(databaseError: DatabaseError) {
+
+                Log.w(TAG, "loadPost:onCancelled", databaseError.toException())
+            }
+        }
+        FirebaseRef.userInfoRef.child(uid).addValueEventListener(postListener)
+    }
+
+    private fun getUserDataList(currentUserGender: String) {
         val postListener = object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
 
@@ -88,8 +134,14 @@ class MainActivity : AppCompatActivity() {
                     Log.d(TAG, dataModel.toString())
 
                     val user = dataModel.getValue(UserDataModel::class.java)
-                    userDataList.add(user!!)
-                    Log.d(TAG, user.nickname.toString())
+
+                    if (user!!.gender.toString().equals(currentUserGender)) {
+
+                    } else {
+                        userDataList.add(user!!)
+                    }
+
+
                 }
                 cardStackAdapter.notifyDataSetChanged()
             }
